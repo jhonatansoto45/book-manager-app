30c504f4c7b71d02d1e9d544c0508a1696bef416
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2025-04-01 16:55:48 -0500

    Implementation of routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './books/pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./books/pages/dashboard/dashboard.component').then(
+        (m) => m.DashboardComponent
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
