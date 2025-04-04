e88ee3e753cd0999756d4aeb48322924d007ebba
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2025-04-03 19:59:54 -0500

    Update html in ts

--- src/app/shared/components/type-section/type-section.component.ts
+++ src/app/shared/components/type-section/type-section.component.ts
@@ -5,12 +5,13 @@ import { Component, input } from '@angular/core';
   imports: [],
   template: `
     <div class="type-section w-full flex justify-center items-center gap-2">
-      <div class="line w-8 bg-white h-1"></div>
-      <p class="text-sm text-white font-bold">Acerca de</p>
-      <div class="line w-8 bg-white h-1"></div>
+      <div class="line w-8  h-1" [style.background-color]="color()"></div>
+      <p class="text-sm text-{{ color() }} font-bold">Acerca de</p>
+      <div class="line w-8  h-1" [style.background-color]="color()"></div>
     </div>
   `,
 })
 export class TypeSectionComponent {
   type = input.required<string>();
+  color = input<string>('white');
 }
