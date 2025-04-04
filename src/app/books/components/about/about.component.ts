860f4c87df353734dad67f761095e15b9ae9f423
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2025-04-03 19:59:25 -0500

    Delete component

--- src/app/books/components/about/about.component.ts
+++ src/app/books/components/about/about.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { ServicesComponent } from '../services/services.component';
 import { TypeSectionComponent } from '../../../shared/components/type-section/type-section.component';
 
 @Component({
   selector: 'app-about',
-  imports: [ServicesComponent, TypeSectionComponent],
+  imports: [TypeSectionComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
 })
