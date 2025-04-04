96216a5678c037913f620b1110c9bbf230020cb2
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2025-04-03 20:00:06 -0500

    Mockup card services

--- src/app/books/components/services/services.component.ts
+++ src/app/books/components/services/services.component.ts
@@ -1,11 +1,50 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faSquare, faCheckSquare } from '@fortawesome/free-solid-svg-icons';
+import { TypeSectionComponent } from '../../../shared/components/type-section/type-section.component';
+interface Services {
+  title: string;
+  description: string;
+  icon: any;
+}
 
 @Component({
   selector: 'app-services',
-  imports: [],
+  imports: [TypeSectionComponent, FontAwesomeModule],
   templateUrl: './services.component.html',
-  styleUrl: './services.component.css'
+  styleUrl: './services.component.css',
 })
 export class ServicesComponent {
-
+  services: Services[] = [
+    {
+      icon: faSquare,
+      title: 'Lorem ipsum dolor sit amet, consectetur adip',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+      icon: faCheckSquare,
+      title: 'Lorem ipsum dolor sit amet, consectetur adip',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+      icon: faCheckSquare,
+      title: 'Lorem ipsum dolor sit amet, consectetur adip',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+      icon: faSquare,
+      title: 'Lorem ipsum dolor sit amet, consectetur adip',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+      icon: faSquare,
+      title: 'Lorem ipsum dolor sit amet, consectetur adip',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+      icon: faCheckSquare,
+      title: 'Lorem ipsum dolor sit amet, consectetur adip',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+  ];
 }
