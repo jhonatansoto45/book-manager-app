7ce6536e26ba77faf228caca43f66f681fa0bd74
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2025-04-04 21:01:00 -0500

    Create section contact

--- src/app/books/components/contact/contact.component.ts
+++ src/app/books/components/contact/contact.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { TypeSectionComponent } from '../../../shared/components/type-section/type-section.component';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [TypeSectionComponent],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrl: './contact.component.css',
 })
-export class ContactComponent {
-
-}
+export class ContactComponent {}
