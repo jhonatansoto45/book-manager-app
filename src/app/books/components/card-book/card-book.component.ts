42456ebab6244fab26f282aa3470fee3b75959a5
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2025-04-08 16:56:52 -0500

    Create modal

--- src/app/books/components/card-book/card-book.component.ts
+++ src/app/books/components/card-book/card-book.component.ts
@@ -1,12 +1,28 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
+import { JsonPipe } from '@angular/common';
 import { Book } from '../../interfaces/books.interfaces';
 
 @Component({
   selector: 'app-card-book',
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './card-book.component.html',
   styleUrl: './card-book.component.css',
 })
 export class CardBookComponent {
   book = input.required<Book>();
+
+  modal = signal(false);
+
+  onViewBook(book: Book): void {
+    // this.modal.update((current) => !current);
+    this.controlModal();
+  }
+
+  controlModal(): void {
+    this.modal.update((current) => !current);
+  }
+
+  getRatingArray(rating: number): any[] {
+    return Array(Math.floor(rating));
+  }
 }
